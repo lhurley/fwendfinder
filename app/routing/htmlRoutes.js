3fc82769fd81665bb2463f6eed56b6e4bba54ad8
Lizzie Hurley <l.hurley58@gmail.com>
2017-09-29 23:48:22 -0500

    links

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,7 +11,7 @@ var path = require("path");
 
 module.exports = function(app) {
 	app.get("/survey", function(req, res) {
-	  res.sendFile(path.join(__dirname, "../public/survey.html"));
+	  res.sendFile(path.join(__dirname, "../public/survery.html"));
 	});
 
 	app.get("*", function(req, res) {
